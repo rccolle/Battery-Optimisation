b890d4417d9d24ca7e80e932aae9835df30d0fba
Reinzy Colle <reinzy.colle@gmail.com>
2024-08-21 15:03:51 +1000

    Added cycle cost to optimisation method

--- algorithms/battery_optimise.py
+++ algorithms/battery_optimise.py
@@ -7,7 +7,14 @@ logging.getLogger('pyomo.core').setLevel(logging.ERROR)
 from pyomo.environ import *
 import highspy
 
-def battery_optimisation(datetime, spot_price, initial_capacity=0, include_revenue=True, solver: str='appsi_highs'):
+def battery_optimisation(
+        datetime,
+        spot_price,
+        initial_capacity=0, 
+        cycle_cost = 0,
+        daily_cycle_limit = 1.0,
+        include_revenue=True, 
+        solver: str='appsi_highs'):
     """
     Determine the optimal charge and discharge behavior of a battery based 
     in Victoria. Assuming pure foresight of future spot prices over every 
@@ -29,14 +36,14 @@ def battery_optimisation(datetime, spot_price, initial_capacity=0, include_reven
     of each half-hour period and battery's raw power for each half-hour priod
     """
     # Battery's technical specification
-    MIN_BATTERY_CAPACITY = 0
-    MAX_BATTERY_CAPACITY = 10
-    MAX_RAW_POWER = 5
+    MIN_BATTERY_CAPACITY = 0.0
+    MAX_BATTERY_CAPACITY = 10.0
+    MAX_RAW_POWER = 5.0
     INITIAL_CAPACITY = initial_capacity # Default initial capacity will assume to be 0
     EFFICIENCY = 0.9
     MLF = 0.991 # Marginal Loss Factor
-    DAILY_CYCLE_LIMIT = 1.0 # Expressed as equivalent daily limit
-    HORIZON_CYCLE_LIMIT = DAILY_CYCLE_LIMIT * ((datetime.iloc[-1] - datetime.iloc[0]).days + 1)
+    DAILY_CYCLE_LIMIT = 2.0 # Expressed as equivalent daily limit
+    HORIZON_CYCLE_LIMIT = daily_cycle_limit * ((datetime.iloc[-1] - datetime.iloc[0]).days + 1)
 
     df = pd.DataFrame({'datetime': datetime, 'spot_price': spot_price}).reset_index(drop=True)
     df['period'] = df.index
@@ -55,12 +62,14 @@ def battery_optimisation(datetime, spot_price, initial_capacity=0, include_reven
     battery.Charge_power = Var(battery.Period, bounds=(0, MAX_RAW_POWER))
     battery.Discharge_power = Var(battery.Period, bounds=(0, MAX_RAW_POWER))
     battery.Cycles = Var(battery.Period, bounds=(0, HORIZON_CYCLE_LIMIT))
+    battery.Cycle_cost = Var(battery.Period)
 
     # Set constraints for the battery
     # Defining the battery objective (function to be maximise)
     def maximise_profit(battery):
         rev = sum(df.spot_price[i] * (battery.Discharge_power[i] / 12) * MLF for i in battery.Period)
         cost = sum(df.spot_price[i] * (battery.Charge_power[i] / 12) / MLF for i in battery.Period)
+        cost += sum(battery.Cycle_cost[i] for i in battery.Period)
         return rev - cost
 
     # Make sure the battery does not charge above the limit
@@ -96,7 +105,14 @@ def battery_optimisation(datetime, spot_price, initial_capacity=0, include_reven
             return battery.Cycles[i] == 0
         return battery.Cycles[i] == (battery.Cycles[i-1]
                                     + ((battery.Charge_power[i] / 12 * EFFICIENCY)
-                                       + (battery.Discharge_power[i] / 12)) / 2 / MAX_BATTERY_CAPACITY)
+                                       + (battery.Discharge_power[i] / 12 / EFFICIENCY)) / 2 / MAX_BATTERY_CAPACITY)
+    
+    # Define cycle cost
+    def incurred_cycle_cost(battery, i):
+        if i == battery.Period.first():
+            return battery.Cycle_cost[i] == battery.Cycles[i] * cycle_cost
+        return battery.Cycle_cost[i] == cycle_cost * (battery.Cycles[i] - battery.Cycles[i-1])
+        
 
     # Set constraint and objective for the battery
     battery.capacity_constraint = Constraint(battery.Period, rule=capacity_constraint)
@@ -104,6 +120,8 @@ def battery_optimisation(datetime, spot_price, initial_capacity=0, include_reven
     battery.over_discharge = Constraint(battery.Period, rule=over_discharge)
     battery.negative_discharge = Constraint(battery.Period, rule=negative_discharge)
     battery.cycling_constraint = Constraint(battery.Period, rule=cycling_constraint)
+    battery.cycle_cost_incurred = Constraint(battery.Period, rule=incurred_cycle_cost)
+
     battery.objective = Objective(rule=maximise_profit, sense=maximize)
 
     # Maximise the objective
@@ -125,25 +143,21 @@ def battery_optimisation(datetime, spot_price, initial_capacity=0, include_reven
     
     # make sure it does not discharge & charge at the same time
     if not len(result[(result.charge_power != 0) & (result.discharge_power != 0)]) == 0:
-        print('Ops! The battery discharges & charges concurrently, the result has been returned')
+        print('Oops! The battery discharges & charges concurrently, the result has been returned')
         return result
     
     # convert columns charge_power & discharge_power to power
     result['power'] = np.where((result.charge_power > 0), 
                                 -result.charge_power, 
                                 result.discharge_power)
+
     
-    # calculate market dispatch
-    result['market_dispatch'] = np.where(result.power < 0,
-                                         result.power / 12,
-                                         result.power / 12 * EFFICIENCY)
-    
-    result = result[['spot_price', 'power', 'market_dispatch', 'opening_capacity', 'cycles']]
+    result = result[['spot_price', 'power', 'opening_capacity', 'cycles']]
     
     # calculate revenue
     if include_revenue:
-        result['revenue'] = np.where(result.market_dispatch < 0, 
-                              result.market_dispatch * result.spot_price / MLF,
-                              result.market_dispatch * result.spot_price * MLF)
+        result['revenue'] = np.where(result.power < 0,
+                                     result.power * result.spot_price / MLF,
+                                     result.power * result.spot_price * MLF) / 12
     
     return result
